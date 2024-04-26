d35d6987ce8774b177834fab878b8a2c9718fb58
rocknrollo <159512620+rocknrollo@users.noreply.github.com>
2024-04-27 02:14:01 +1000

    Update prog4_9.c
    updated code with some comments

--- loops_chapt4/do_while_loops/prog4_9.c
+++ loops_chapt4/do_while_loops/prog4_9.c
@@ -3,14 +3,24 @@
 int main ()
 {
     int number, reverse;
-
+// ask user for number input
     printf("enter a number: ");
     scanf ("%i", &number);
-
+/* the program is guaranteed to run atleast once.
+    the program statement is executed first then the while
+    statement checks if the condition is met. if the condition is false
+    the program ends if the condition is true the ps is tried again */
     do
     {
+        //get remainder of number divided by 10
+        
         reverse = number % 10;
+        
+        //prints the remainder
+        
         printf("%i", reverse);
+        
+        //number is recalibrated to be number/10 to restart the loop
         number /= 10;
     
     } while (number !=0);
