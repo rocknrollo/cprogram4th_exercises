429db5dfefd8d741eaec85157c3798b3e9dfb17d
rocknrollo <damien.itiogen@outlook.com>
2024-04-29 00:08:21 +1000

    prime numbers

---
+++ making_decisions/prog5_10.c
@@ -0,0 +1,30 @@
+// program to generate prime numbers
+
+#include <stdio.h>
+
+int main (void)
+{
+    int p, d;
+    _Bool isPrime;
+
+    for(d = 2; p < 50; ++p)
+    {
+        isPrime = 1;
+
+        for (d = 2; d < p; ++d)
+        {
+            if (p % d == 0)
+            {
+                isPrime = 0;
+            }
+            if (isPrime !=0)
+            {
+                printf("%i ", p);
+            }
+        }
+        
+    }
+    printf("\n");
+    return 0;
+
+}
